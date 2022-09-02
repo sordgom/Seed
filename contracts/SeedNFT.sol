8d7122cdd40af0f7bf7a1127676eb699b691cb46
Sordgom <aymaneigmiden@gmail.com>
2022-09-03 02:09:07 +0800

    4

--- contracts/SeedNFT.sol
+++ contracts/SeedNFT.sol
@@ -5,18 +5,32 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "./SeedBatch.sol";
-contract SeedNFT is SeedBatch{
+
+contract SeedNFT is SeedBatch {
+     using Strings for string;
 
     string private _eventName;
     string private _symbol;
     string private _baseUri;
-     constructor(string memory eventName, string memory symbol, string memory Uri) {
+    uint256 private _currentTokenID = 0;
+    uint256 private _timeCreated;
+    uint256 private _timePeriod;
+
+    constructor(
+        string memory eventName,
+        string memory symbol,
+        string memory Uri,
+        uint256 memory timePeriod
+    ) {
         _eventName = eventName;
         _symbol = symbol;
         _baseUri = Uri;
+        _timeCreated = block.timestamp;
+        _timePeriod = timePeriod * 24 * 60 * 60;// days to seconds
+        setTokenUri(_currentTokenID, _baseUri);
     }
 
-     function eventName() external view returns (string memory) {
+    function eventName() external view returns (string memory) {
         return _eventName;
     }
 
@@ -24,11 +38,39 @@ contract SeedNFT is SeedBatch{
         return _symbol;
     }
 
-    function mint(address account, uint256 id, uint256 amount) override public onlyOwner {
-        mint( account,  id,  amount, _baseUri);
+//Change the uri 
+     function uri(uint256 _eventId) public view returns (string memory) {
+        return Strings.strConcat(
+        uri,
+        Strings.uint2str(_eventId)
+        );
     }
 
-    function bulk_mint(address[] memory accounts, uint256 id) override public onlyOwner {
-        bulk_mint( accounts,  id, _baseUri);
+    function mint(address account, uint256 id, uint256 amount) public override onlyOwner {
+        require(
+            block.timestamp <= _timeCreated + _timePeriod,
+            "You can't mint this token anymore"
+        );
+        mint(account, id, amount, _baseUri);
+    }
+
+    function bulk_mint(address[] memory accounts, uint256 id)
+        public
+        override
+        onlyOwner
+    {
+        require(
+            block.timestamp <= _timeCreated + _timePeriod,
+            "You can't mint this token anymore"
+        );
+        bulk_mint(accounts, id, _baseUri);
+    }
+
+    function _getNextTokenID() private view returns (uint256) {
+        return _currentTokenID.add(1);
+    }
+
+    function _incrementTokenTypeId() private {
+        _currentTokenID++;
     }
 }
